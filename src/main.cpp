7196a11d9f884bb490f9e668b5dec03ee7bc6aec
igors48 <nmds48@gmail.com>
2021-05-13 07:28:58 +0300

    test commit

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ void setup() {
     watch->tft->fillScreen(TFT_BLACK);
     watch->tft->setTextFont(2);
     watch->tft->setTextSize(1);
-    watch->tft->setTextColor(TFT_WHITE);
+    watch->tft->setTextColor(TFT_GREEN);
     watch->tft->setCursor(0, 0);
     watch->tft->println(F("Hello T-World"));
 }
