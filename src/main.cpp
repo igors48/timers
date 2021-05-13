536851eb3aff3b490e3f417a256478bdd60b4f77
igors48 <nmds48@gmail.com>
2021-05-13 07:24:26 +0300

    initial platformio

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+// Select you T-Watch in the platformio.ini file 
+#include <LilyGoWatch.h>
+
+// C++ object which will allow access to the functions of the Watch 
+TTGOClass *watch;
+
+void setup() {
+    // Get Watch object and set up the display 
+    watch = TTGOClass::getWatch();
+    watch->begin();     
+    watch->openBL();
+    
+    // Use SPI_eTFT library to display text on screen 
+    watch->tft->fillScreen(TFT_BLACK);
+    watch->tft->setTextFont(2);
+    watch->tft->setTextSize(1);
+    watch->tft->setTextColor(TFT_WHITE);
+    watch->tft->setCursor(0, 0);
+    watch->tft->println(F("Hello T-World"));
+}
+
+void loop() {}
