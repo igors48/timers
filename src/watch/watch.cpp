5bd41049253f0353442e1a74708369f67dfa8e31
igors48 <nmds48@gmail.com>
2021-07-29 06:54:40 +0300

    #13 state rendering
    time fixed

--- src/watch/watch.cpp
+++ src/watch/watch.cpp
@@ -9,26 +9,14 @@ void watchInit()
     watch = TTGOClass::getWatch();
     watch->begin();
 
-    auto tnow = watch->rtc->formatDateTime(PCF_TIMEFORMAT_HMS);
-    Serial.println(tnow);
-
-    watch->rtc->check();
-    watch->rtc->syncToSystem();
-
-    tnow = watch->rtc->formatDateTime(PCF_TIMEFORMAT_HMS);
-    Serial.println(tnow);    
+    RTC_Date compiled = RTC_Date(__DATE__, __TIME__);
+    watch->rtc->setDateTime(compiled);
 
     watch->power->enableIRQ(AXP202_PEK_SHORTPRESS_IRQ | AXP202_VBUS_REMOVED_IRQ | AXP202_VBUS_CONNECT_IRQ | AXP202_CHARGING_IRQ, true);
     watch->power->adc1Enable(AXP202_VBUS_VOL_ADC1 | AXP202_VBUS_CUR_ADC1 | AXP202_BATT_CUR_ADC1 | AXP202_BATT_VOL_ADC1, true);
     watch->power->clearIRQ();
 
-
     WiFi.mode(WIFI_OFF);
-
-    // watch->tft->setCursor(8, 8);
-    // watch->tft->setTextSize(3);
-    // watch->tft->setTextFont(2);
-    // watch->tft->print("ON");
 }
 
 void watchAfterWakeUp()
