49987b7589bcd653c3619fa1dca16effc4d54a15
igors48 <nmds48@gmail.com>
2021-08-02 07:38:07 +0300

    #15 ui component
    initial

---
+++ src/component/component.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../task/watchState.hpp"
+#include "../watch/tft.hpp"
+
+typedef void (*Render)(WatchState watchState, TftApi *tftApi);
+
+typedef struct 
+{
+    Render render;    
+};
