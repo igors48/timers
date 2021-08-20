8449eb33835aa08eb3e4a67f094b68a4a6450806
igors48 <nmds48@gmail.com>
2021-08-20 07:16:38 +0300

    #21 reset step counter once a day
    verified commit
    todo - reset each 24 hours

--- src/task/stepCounterReset.cpp
+++ src/task/stepCounterReset.cpp
@@ -11,7 +11,9 @@ void stepCounterReset(StepCounterResetParameters *p)
         p->bmaApi->resetStepCounter();
         Date now = p->rtcApi->getDate();
         unsigned long nextExecution = secondsToNextHourStart(now) * 1000;
+        //unsigned long nextExecution = 30000;
         p->systemApi->give(p->watchMutex);
+        p->systemApi->log(STEP_COUNTER_RESET, "lastWakeTime %ld, nextExecution %ld", *(p->lastWakeTime), nextExecution);
         p->systemApi->delayUntil(p->lastWakeTime, nextExecution);
     }
     else
