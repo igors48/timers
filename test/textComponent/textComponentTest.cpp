c97bd69186e9868369d13cd3a0d4064290b1225f
igors48 <nmds48@gmail.com>
2021-11-18 07:07:45 +0200

    #31 generic text component
    text component test
    todo merge

---
+++ test/textComponent/textComponentTest.cpp
@@ -0,0 +1,106 @@
+#include <unity.h>
+
+#include "task/watchStateProducer.cpp"
+#include "component/textComponent.cpp"
+#include "component/component.cpp"
+
+Component component;
+TextState textState;
+WatchState watchState;
+
+bool provideCalled;
+
+void provideStub(TextState *state, WatchState *watchState)
+{
+    provideCalled = true;
+}
+
+void setUp(void)
+{
+    textState = {};
+    strcpy(textState.content, "a");
+    strcpy(textState._content, "a");
+    textState.size = 1;
+    textState._size = 1;
+    textState.fontColor = 2;
+    textState._fontColor = 2;
+    textState.backColor = 3;
+    textState._backColor = 3;
+    textState.font = 4;
+    textState._font = 4;
+    textState.provide = provideStub;
+    
+    component = {};
+    component.state = &textState;
+    
+    watchState = {};
+    
+    provideCalled = false;
+}
+
+void whenNewState()
+{
+    textComponentNewState(&component, &watchState);
+
+    TEST_ASSERT_EQUAL_UINT8(1, provideCalled); // THEN provide function called
+}
+
+void whenStateNoChanged()
+{
+    bool changed = textComponentNewState(&component, &watchState);
+
+    TEST_ASSERT_EQUAL_UINT8(0, changed); // THEN state no changed
+}
+
+void whenContentChanged()
+{
+    strcpy(textState.content, "b");
+    bool changed = textComponentNewState(&component, &watchState);
+
+    TEST_ASSERT_EQUAL_UINT8(1, changed); // THEN state changed
+}
+
+void whenSizeChanged()
+{
+    textState.size = 48;
+    bool changed = textComponentNewState(&component, &watchState);
+
+    TEST_ASSERT_EQUAL_UINT8(1, changed); // THEN state changed
+}
+
+void whenFontColorChanged()
+{
+    textState.fontColor = 48;
+    bool changed = textComponentNewState(&component, &watchState);
+
+    TEST_ASSERT_EQUAL_UINT8(1, changed); // THEN state changed
+}
+
+void whenBackColorChanged()
+{
+    textState.backColor = 48;
+    bool changed = textComponentNewState(&component, &watchState);
+
+    TEST_ASSERT_EQUAL_UINT8(1, changed); // THEN state changed
+}
+
+void whenFontChanged()
+{
+    textState.font = 48;
+    bool changed = textComponentNewState(&component, &watchState);
+
+    TEST_ASSERT_EQUAL_UINT8(1, changed); // THEN state changed
+}
+
+int main()
+{
+    UNITY_BEGIN();
+    RUN_TEST(whenNewState);
+    RUN_TEST(whenStateNoChanged);
+    RUN_TEST(whenContentChanged);
+    RUN_TEST(whenSizeChanged);
+    RUN_TEST(whenFontColorChanged);
+    RUN_TEST(whenBackColorChanged);
+    RUN_TEST(whenFontChanged);
+    UNITY_END();
+}
