7e7a8c5fa03fd2f06fac20a926d63e6a6a36cb6f
igors48 <nmds48@gmail.com>
2021-12-23 07:29:34 +0200

    #35 sound
    alarm commit
    sound player
    todo delete i2sNum and waitInMillis from signed int (*Write)
    todo buffer[samplesCount * 4];  4 because in bytes in soundPlayer
    todo finish player

--- src/task/soundPlayer.cpp
+++ src/task/soundPlayer.cpp
@@ -2,11 +2,41 @@
 
 static const char SOUND_PLAYER[] = "soundPlayer";
 
+static const unsigned short waveLength = 10;
+static const unsigned short volume = 2048;
+static const unsigned short samplesCount = 2000; // todo 1/4 sec -> 8000 / 4. it depends on sample rate defined not here. need to refactor
+static unsigned int buffer[samplesCount];
+
+static void beep(SoundPlayerParameters *p)
+{
+    signed short outputValue = volume;
+    unsigned short timeAtPeakOrTrough = waveLength;
+    for (unsigned short i = 0; i < samplesCount; i++)
+    {
+        if (timeAtPeakOrTrough == 0)
+        {
+            if (outputValue == volume)
+            {
+                outputValue = -volume;
+            }
+            else
+            {
+                outputValue = volume;
+            }
+            timeAtPeakOrTrough = waveLength;
+        }
+        timeAtPeakOrTrough--;
+        unsigned int output = (outputValue << 16) | (outputValue & 0xffff);
+        buffer[i] = output;
+    }
+    (p->i2sApi->write)();
+}
+
 void soundPlayer(SoundPlayerParameters *p)
 {
     unsigned char code;
     if ((p->systemApi->queueReceive)(p->queue, &code, 0xffffffffUL)) // todo consider how to use the portMAX_DELAY const here
     {
-
+        beep(p);
     }
 }
--- src/task/soundPlayer.hpp
+++ src/task/soundPlayer.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 #include "../system/system.hpp"
+#include "../watch/i2s.hpp"
 
 typedef struct 
 {
     void *queue;
+    I2sApi *i2sApi;
     SystemApi *systemApi;    
 } SoundPlayerParameters;
 
