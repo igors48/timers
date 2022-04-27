d7337b8b8f89e194d2175a51875f4287eef987ab
igors48 <nmds48@gmail.com>
2022-04-27 07:50:39 +0300

    #42 clock app
    cleanups
    todo test

--- src/apps/screen.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "core/system/sound.hpp"
-#include "core/component/component.hpp"
-
-typedef void (*SetActiveTile)(unsigned short activeTile);
-
-Component createScreen(SoundApi *soundApi);
