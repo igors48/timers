f7bf7f34d5777521d054dcdddb3b3e3b2d2eb98a
igors48 <nmds48@gmail.com>
2022-05-09 07:46:44 +0300

    #43 button component improvements
    test completed

--- test/buttonComponent/buttonComponentTest.cpp
+++ test/buttonComponent/buttonComponentTest.cpp
@@ -3,10 +3,18 @@
 #include "core/component/component.cpp"
 #include "core/component/buttonComponent.cpp"
 
-static char TEST[] = "test";
-static bool handlerCalled;
-static ButtonComponentState state;
-static Component button;
+char TEST[] = "test";
+bool handlerCalled;
+ButtonComponentState state;
+Component button;
+
+unsigned int firstTouchTick;
+unsigned int secondTouchTick;
+unsigned int thirdTouchTick;
+unsigned int touchBeforeFirstDelay;
+unsigned int touchAfterFirstDelay;
+unsigned int touchBeforeRepeatDelay;
+unsigned int touchAfterRepeatDelay;
 
 void handlerStub()
 {
@@ -19,14 +27,22 @@ void setUp(void)
 
     state = createButtonState(TEST, EG_ONCE, handlerStub);
     button = createButtonComponent(10, 10, 10, 10, &state);
+
+    firstTouchTick = 48;
+    secondTouchTick = firstTouchTick + 10;
+    thirdTouchTick = secondTouchTick + 10;
+    touchBeforeFirstDelay = firstTouchTick + state.delayTick - 100;
+    touchAfterFirstDelay = firstTouchTick + state.delayTick + 100;
+    touchBeforeRepeatDelay = touchAfterFirstDelay + state.repeatTick - 50;
+    touchAfterRepeatDelay = touchAfterFirstDelay + state.repeatTick + 50;
 }
 
 void whenTouch()
 {
-    button.onTouch(&button, 15, 15, 48);
+    button.onTouch(&button, 15, 15, firstTouchTick);
 
     TEST_ASSERT_TRUE(state.eventHandlingState == EHS_PRESS); // THEN state is set to press
-    TEST_ASSERT_EQUAL_UINT32(state.firstTouchTick, 48); // THEN first touch tick is stored
+    TEST_ASSERT_EQUAL_UINT32(state.firstTouchTick, firstTouchTick); // THEN first touch tick is stored
     TEST_ASSERT_FALSE(handlerCalled) // THEN handler is not called
 }
 
@@ -34,7 +50,7 @@ void whenTouchDisabled()
 {
     state.mode = BM_DISABLED;
 
-    button.onTouch(&button, 15, 15, 48);
+    button.onTouch(&button, 15, 15, firstTouchTick);
 
     TEST_ASSERT_TRUE(state.eventHandlingState == EHS_IDLE); // THEN state is not changed
     TEST_ASSERT_EQUAL_UINT32(state.firstTouchTick, 0); // THEN first touch tick is not changed
@@ -43,9 +59,9 @@ void whenTouchDisabled()
 
 void whenReleasedOnMe()
 {
-    button.onTouch(&button, 15, 15, 48);
+    button.onTouch(&button, 15, 15, firstTouchTick);
 
-    button.onRelease(&button, 15, 15, 2048);
+    button.onRelease(&button, 15, 15, secondTouchTick);
 
     TEST_ASSERT_TRUE(handlerCalled) // THEN handler is called
     TEST_ASSERT_TRUE(state.eventHandlingState == EHS_IDLE); // THEN state is set to idle
@@ -54,11 +70,11 @@ void whenReleasedOnMe()
 void whenReleasedOnMeRepeated()
 {
     state.eventGenerate = EG_REPEAT;
-    button.onTouch(&button, 15, 15, 48);
-    button.onMove(&button, 15, 15, 2048);
+    button.onTouch(&button, 15, 15, firstTouchTick);
+    button.onMove(&button, 15, 15, touchAfterFirstDelay);
     handlerCalled = false; // need to reset because it is set to true when first repeat
 
-    button.onRelease(&button, 15, 15, 48);
+    button.onRelease(&button, 15, 15, touchAfterRepeatDelay);
 
     TEST_ASSERT_FALSE(handlerCalled) // THEN handler is not called
     TEST_ASSERT_TRUE(state.eventHandlingState == EHS_IDLE); // THEN state is set to idle
@@ -68,7 +84,7 @@ void whenReleasedOnMeDisabled()
 {
     state.mode = BM_DISABLED;
 
-    button.onRelease(&button, 15, 15, 48);
+    button.onRelease(&button, 15, 15, firstTouchTick);
 
     TEST_ASSERT_FALSE(handlerCalled) // THEN handler is not called
     TEST_ASSERT_TRUE(state.eventHandlingState == EHS_IDLE); // THEN state is set to idle
@@ -76,12 +92,12 @@ void whenReleasedOnMeDisabled()
 
 void whenReleasedOnMeRepeatedDisabled()
 {
-    button.onTouch(&button, 15, 15, 48);
-    button.onMove(&button, 15, 15, 2048);
+    button.onTouch(&button, 15, 15, firstTouchTick);
+    button.onMove(&button, 15, 15, touchAfterFirstDelay);
     handlerCalled = false; // need to reset because it is set to true when first repeat
     state.mode = BM_DISABLED;
 
-    button.onRelease(&button, 15, 15, 48);
+    button.onRelease(&button, 15, 15, touchAfterRepeatDelay);
 
     TEST_ASSERT_FALSE(handlerCalled) // THEN handler is not called
     TEST_ASSERT_TRUE(state.eventHandlingState == EHS_IDLE); // THEN state is set to idle
@@ -89,7 +105,7 @@ void whenReleasedOnMeRepeatedDisabled()
 
 void whenReleasedNotOnMe()
 {
-    button.onRelease(&button, 115, 115, 48);
+    button.onRelease(&button, 115, 115, firstTouchTick);
 
     TEST_ASSERT_FALSE(handlerCalled) // THEN handler is not called
     TEST_ASSERT_TRUE(state.eventHandlingState == EHS_IDLE); // THEN state is set to idle
@@ -98,11 +114,11 @@ void whenReleasedNotOnMe()
 void whenReleasedNotOnMeRepeated()
 {
     state.eventGenerate = EG_REPEAT;
-    button.onTouch(&button, 15, 15, 48);
-    button.onMove(&button, 15, 15, 2048);
+    button.onTouch(&button, 15, 15, firstTouchTick);
+    button.onMove(&button, 15, 15, touchAfterFirstDelay);
     handlerCalled = false; // need to reset because it is set to true when first repeat
 
-    button.onRelease(&button, 115, 115, 48);
+    button.onRelease(&button, 115, 115, touchAfterRepeatDelay);
 
     TEST_ASSERT_FALSE(handlerCalled) // THEN handler is not called
     TEST_ASSERT_TRUE(state.eventHandlingState == EHS_IDLE); // THEN state is set to idle
@@ -110,38 +126,38 @@ void whenReleasedNotOnMeRepeated()
 
 void whenMoveOutside()
 {
-    button.onTouch(&button, 15, 15, 48);
+    button.onTouch(&button, 15, 15, firstTouchTick);
 
-    button.onMove(&button, 115, 115, 148);
+    button.onMove(&button, 115, 115, secondTouchTick);
 
     TEST_ASSERT_TRUE(state.eventHandlingState == EHS_LEAVE); // THEN state is set to leave
 }
 
 void whenMoveOutsideDisabled()
 {
-    button.onTouch(&button, 15, 15, 48);
+    button.onTouch(&button, 15, 15, firstTouchTick);
 
     state.mode = BM_DISABLED;
-    button.onMove(&button, 115, 115, 148);
+    button.onMove(&button, 115, 115, secondTouchTick);
 
     TEST_ASSERT_TRUE(state.eventHandlingState == EHS_IDLE); // THEN state is set to idle
 }
 
 void whenMoveInside()
 {
-    button.onTouch(&button, 15, 15, 48);
+    button.onTouch(&button, 15, 15, firstTouchTick);
 
-    button.onMove(&button, 16, 16, 148);
+    button.onMove(&button, 16, 16, secondTouchTick);
 
     TEST_ASSERT_TRUE(state.eventHandlingState == EHS_PRESS); // THEN state is set to press
 }
 
 void whenMoveInsideDisabled()
 {
-    button.onTouch(&button, 15, 15, 48);
+    button.onTouch(&button, 15, 15, firstTouchTick);
 
     state.mode = BM_DISABLED;
-    button.onMove(&button, 16, 16, 148);
+    button.onMove(&button, 16, 16, secondTouchTick);
 
     TEST_ASSERT_TRUE(state.eventHandlingState == EHS_IDLE); // THEN state is set to idle
 }
@@ -149,22 +165,48 @@ void whenMoveInsideDisabled()
 void whenMoveInsideRepeatedBeforeDelay()
 {
     state.eventGenerate = EG_REPEAT;
-    button.onTouch(&button, 15, 15, 48);
-    button.onMove(&button, 16, 16, 148);
+    button.onTouch(&button, 15, 15, firstTouchTick);
+    button.onMove(&button, 16, 16, touchBeforeFirstDelay);
 
     TEST_ASSERT_TRUE(state.eventHandlingState == EHS_PRESS); // THEN state is still press
-    TEST_ASSERT_EQUAL_UINT32(state.lastRepeatTick, 0); // THEN first touch tick is not changed
+    TEST_ASSERT_EQUAL_UINT32(state.lastRepeatTick, 0); // THEN last repeat tick is not changed
     TEST_ASSERT_FALSE(handlerCalled); // THEN no handler is called
 }
 
 void whenMoveInsideRepeatedAfterDelay()
 {
     state.eventGenerate = EG_REPEAT;
-    button.onTouch(&button, 15, 15, 48);
-    button.onMove(&button, 16, 16, 2048);
+    button.onTouch(&button, 15, 15, firstTouchTick);
+    button.onMove(&button, 16, 16, touchAfterFirstDelay);
 
     TEST_ASSERT_TRUE(state.eventHandlingState == EHS_REPEAT); // THEN state is changed to repeat
-    TEST_ASSERT_EQUAL_UINT32(state.lastRepeatTick, 2048); // THEN first touch tick is not changed
+    TEST_ASSERT_EQUAL_UINT32(state.lastRepeatTick, touchAfterFirstDelay); // THEN last repeat tick is updated
+    TEST_ASSERT_TRUE(handlerCalled); // THEN handler is called
+}
+
+void whenMoveInsideRepeatedBeforeRepeatDelay()
+{
+    state.eventGenerate = EG_REPEAT;
+    button.onTouch(&button, 15, 15, firstTouchTick);
+    button.onMove(&button, 16, 16, touchAfterFirstDelay);
+    handlerCalled = false;
+    button.onMove(&button, 17, 17, touchBeforeRepeatDelay);
+
+    TEST_ASSERT_TRUE(state.eventHandlingState == EHS_REPEAT); // THEN state is still repeat
+    TEST_ASSERT_EQUAL_UINT32(state.lastRepeatTick, touchAfterFirstDelay); // THEN last repeat tick is not changed
+    TEST_ASSERT_FALSE(handlerCalled); // THEN no handler is called
+}
+
+void whenMoveInsideRepeatedAfterRepeatDelay()
+{
+    state.eventGenerate = EG_REPEAT;
+    button.onTouch(&button, 15, 15, firstTouchTick);
+    button.onMove(&button, 16, 16, touchAfterFirstDelay);
+    handlerCalled = false;
+    button.onMove(&button, 17, 17, touchAfterRepeatDelay);
+
+    TEST_ASSERT_TRUE(state.eventHandlingState == EHS_REPEAT); // THEN state is still repeat
+    TEST_ASSERT_EQUAL_UINT32(state.lastRepeatTick, touchAfterRepeatDelay); // THEN last repeat tick is updated
     TEST_ASSERT_TRUE(handlerCalled); // THEN handler is called
 }
 
@@ -185,5 +227,7 @@ int main()
     RUN_TEST(whenMoveInsideDisabled);
     RUN_TEST(whenMoveInsideRepeatedBeforeDelay);
     RUN_TEST(whenMoveInsideRepeatedAfterDelay);
+    RUN_TEST(whenMoveInsideRepeatedBeforeRepeatDelay);
+    RUN_TEST(whenMoveInsideRepeatedAfterRepeatDelay);
     UNITY_END();
 }
