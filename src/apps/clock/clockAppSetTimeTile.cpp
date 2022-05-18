81281a9be7ba2d47d79dc296db8174d1e82a64ea
igors48 <nmds48@gmail.com>
2022-05-18 11:33:29 +0300

    #49 hour and minute tile for the clock app. vrfd
    initial

--- src/apps/clock/clockAppSetTimeTile.cpp
+++ src/apps/clock/clockAppSetTimeTile.cpp
@@ -5,24 +5,13 @@
 #include "core/component/textComponent.hpp"
 #include "core/component/group.hpp"
 
-static char PLUS[] = "+";
-static char MINUS[] = "-";
 static char SET[] = "SET";
 
-static const unsigned char COMPONENTS_COUNT = 5;
+static const unsigned char COMPONENTS_COUNT = 2;
 static void* components[COMPONENTS_COUNT];
 
 static TextState* hourMinute;
 static Component* hourMinuteComponent;
-
-static TextState* second;
-static Component* secondComponent;
-
-static ButtonComponentState* hourPlusButtonState;
-static Component* hourPlusButton;
-
-static ButtonComponentState* hourMinusButtonState;
-static Component* hourMinusButton;
 
 static ButtonComponentState* setButtonState;
 static Component* setButton;
@@ -30,21 +19,13 @@ static Component* setButton;
 static GroupState* state;
 static Component* group;
 
-static signed char hourDelta;
-
 static ClockAppApi *api;
 
 static void provideHourMinuteState(TextState *state)
 {
     Date date = api->getDate();
-    unsigned char hour = date.hour + hourDelta;
+    unsigned char hour = date.hour;
     snprintf(state->content, sizeof(state->content), "%02d:%02d", hour, date.minute);
-}
-
-static void provideSecondState(TextState *state)
-{
-    Date date = api->getDate();
-    snprintf(state->content, sizeof(state->content), ":%02d", date.second);
 }
 
 static void onGesture(Component *component, Gesture gesture)
@@ -52,45 +33,23 @@ static void onGesture(Component *component, Gesture gesture)
     api->onGesture(gesture);
 }
 
-static void hourPlus(void *context)
-{
-    hourDelta++;
-}
-
-static void hourMinus(void *context)
-{
-    hourDelta--;
-}
 
 static void setTime(void *context)
 {
-    api->adjDate(hourDelta);
-    hourDelta = 0;
 }
 
 Component* createClockAppSetTimeTile(ClockAppApi *clockAppApi, Factory *factory)
 {
     api = clockAppApi;
 
-    hourDelta = 0;
-
     hourMinute = (factory->createTextStateRef)(7, 1, COLOR_INTERACTION, provideHourMinuteState);
-    second = (factory->createTextStateRef)(7, 1, COLOR_INTERACTION, provideSecondState);
-    hourPlusButtonState = (factory->createButtonStateRef)(PLUS, EG_REPEAT, hourPlus);
-    hourMinusButtonState = (factory->createButtonStateRef)(MINUS, EG_REPEAT, hourMinus);
     setButtonState = (factory->createButtonStateRef)(SET, EG_ONCE, setTime);
 
     hourMinuteComponent = (factory->createTextComponentRef)(10, 90, 140, 48, hourMinute);
-    secondComponent = (factory->createTextComponentRef)(150, 90, 75, 48, second);
-    hourPlusButton = (factory->createButtonComponentRef)(10, 20, 66, 50, hourPlusButtonState);
-    hourMinusButton = (factory->createButtonComponentRef)(10, 180, 66, 50, hourMinusButtonState);
     setButton = (factory->createButtonComponentRef)(80, 20, 66, 50, setButtonState);
 
     components[0] = hourMinuteComponent;
-    components[1] = secondComponent;
-    components[2] = hourPlusButton;
-    components[3] = hourMinusButton;
-    components[4] = setButton;
+    components[1] = setButton;
  
     state = (factory->createGroupStateRef)(COMPONENTS_COUNT, components);
     group = (factory->createGroupComponentRef)(0, 0, state);
