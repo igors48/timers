6facf33d06494ce6b31a7cb5a062825f567da82a
igors48 <nmds48@gmail.com>
2022-05-20 07:50:48 +0300

    #49 hour and minute tile for the clock app
    stepper component test initial
    todo stepper tests

---
+++ test/test_stepperComponent/stepperComponentTest.cpp
@@ -0,0 +1,16 @@
+#include <unity.h>
+
+#include "core/component/stepperComponent.cpp"
+
+StepperComponentState state;
+
+void setUp(void)
+{
+}
+
+int main()
+{
+    UNITY_BEGIN();
+    //RUN_TEST(whenPeriodStartAndLastResetWasDayAgo);
+    UNITY_END();
+}
