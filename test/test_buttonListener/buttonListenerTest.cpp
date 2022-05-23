0b0ecea2fcb7c37e72ccfe7764c34fb524f34f7e
igors48 <nmds48@gmail.com>
2022-05-23 07:15:16 +0300

    #54 power button event handler
    button handler test
    todo improve tiler test

--- test/test_buttonListener/buttonListenerTest.cpp
+++ test/test_buttonListener/buttonListenerTest.cpp
@@ -9,6 +9,8 @@ int watchMutex;
 long lastUserEventTimestamp;
 PowerApi powerApi;
 SystemApi systemApi;
+Tiler tiler;
+bool onButtonCalled;
 
 ButtonListenerParameters p;
 
@@ -22,17 +24,26 @@ bool notShortPress()
     return false;
 }
 
+void onButton()
+{
+    onButtonCalled = true;
+}
+
 void setUp(void)
 {
     powerApi = powerApiMock();
     systemApi = systemApiMock();
     systemApi.time = time;
 
+    onButtonCalled = false;
+    tiler.onButton = onButton;
+
     lastUserEventTimestamp = 0;
 
     p = {
         .watchMutex = &watchMutex,
         .lastUserEventTimestamp = &lastUserEventTimestamp,
+        .tiler = &tiler,
         .powerApi = &powerApi,
         .systemApi = &systemApi};
 }
@@ -42,6 +53,7 @@ void whenItIsShortPress()
     buttonListener(&p);
 
     TEST_ASSERT_EQUAL_UINT32(48, lastUserEventTimestamp); // THEN last user event timestamp changed
+    TEST_ASSERT_TRUE(onButtonCalled); // THEN button handler called
 }
 
 void whenItIsNotShortPress() 
@@ -51,6 +63,7 @@ void whenItIsNotShortPress()
     buttonListener(&p);
 
     TEST_ASSERT_EQUAL_UINT32(0, lastUserEventTimestamp); // THEN last user event timestamp not changed
+    TEST_ASSERT_FALSE(onButtonCalled); // THEN button handler not called
 }
 
 int main()
