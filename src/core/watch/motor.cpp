1433a954c88203f5cb230f39a111a730e296ec41
igors48 <nmds48@gmail.com>
2022-06-10 13:19:24 +0300

    #61 motor pwm channel
    done

--- src/core/watch/motor.cpp
+++ src/core/watch/motor.cpp
@@ -1,14 +1,29 @@
+/**
+ * @file 
+ * @author Igor Usenko (github: igors48)
+ * @brief Motor API implementation
+*/
 #include "motor.hpp"
 #include "ttgo.hpp"
 
-void motorBuzz(int duration)
+/**
+ * @brief default instance of Motor class
+ */
+static Motor motor = Motor(MOTOR_PIN, 4, 100);
+
+/**
+ * @copydoc MotorApi.buzz
+ */
+static void buzz(int duration)
 {
-    watch->motor->onec(duration);
+    motor.onec(duration);
 }
 
 MotorApi watchMotorApi()
 {
+    motor.begin();
+
     return {
-        .buzz = motorBuzz,
+        .buzz = buzz,
     };
 }
