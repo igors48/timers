0730e37a714fa5e836acbdfd5a46f46c6304f680
igors48 <nmds48@gmail.com>
2022-06-18 07:20:22 +0300

    #89 investigate sprites. vrfd
    sprite render optimization

--- src/core/watch/tft.cpp
+++ src/core/watch/tft.cpp
@@ -1,55 +1,69 @@
 #include "tft.hpp"
 
 static TFT_eSprite *sprite;
+static bool dirty;
 
 static void setTextSize(unsigned char s)
 {
+    dirty = true;
     sprite->setTextSize(s);
 }
 
 static void setTextFont(unsigned char f)
 {
+    dirty = true;
     sprite->setTextFont(f);
 }
 
 static void setTextColor(unsigned short f, unsigned short b)
 {
+    dirty = true;
     sprite->setTextColor(f, b);
 }
 
 static void drawString(const char str[], signed int x, signed int y)
 {
+    dirty = true;
     sprite->drawString(str, x, y);
 }
 
 static void drawRect(signed int x, signed int y, signed int w, signed int h, unsigned int color)
 {
+    dirty = true;
     sprite->drawRect(x, y, w, h, color);
 }
 
 static void fillRect(signed int x, signed int y, signed int w, signed int h, unsigned int color)
 {
+    dirty = true;
     sprite->fillRect(x, y, w, h, color);
 }
 
 static void fillRoundRect(signed int x, signed int y, signed int w, signed int h, signed int r, unsigned int color)
 {
+    dirty = true;
     sprite->fillRoundRect(x, y, w, h, r, color);
 }
 
 static void drawFastVLine(signed int x, signed int y, signed int h, unsigned int color)
 {
+    dirty = true;
     sprite->drawFastVLine(x, y, h, color);
 }
 
 static void drawFastHLine(signed int x, signed int y, signed int w, unsigned int color)
 {
+    dirty = true;
     sprite->drawFastHLine(x, y, w, color);
 }
 
 static void pushSprite()
 {
-    sprite->pushSprite(0, 0);
+    if (dirty)
+    {
+        sprite->pushSprite(0, 0);
+    }
+    dirty = false;
 }
 
 TftApi watchTftApi()
@@ -57,6 +71,8 @@ TftApi watchTftApi()
     sprite = new TFT_eSprite(watch->tft);
     sprite->createSprite(240, 240);
 
+    dirty = false;
+    
     return {
         .setTextSize = setTextSize,
         .setTextFont = setTextFont,
